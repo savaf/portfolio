0afcfc044e8d581bf8ec83ffd0bfdf65bb873bfc
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 05:44:03 -0400

    Add AppFooter and FooterCopyright components

---
+++ components/shared/FooterCopyright.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const copyrightDate = new Date().getFullYear();
+</script>
+
+<template>
+  <div class="flex justify-center items-center text-center">
+    <div class="font-general-regular text-lg text-ternary-dark dark:text-ternary-light">&copy; {{ copyrightDate }}</div>
+  </div>
+</template>
