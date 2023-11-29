3aece0221078227aacaff33484dc1af420172a68
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 22:19:20 -0400

    Add ProjectsGrid component to projects page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,1 +1,11 @@
-<template>projects</template>
+<script></script>
+
+<template>
+  <main class="isolate w-full bg-secondary-dark mt-16 sm:mt-32 pb-16 sm:pb-32">
+    <div class="container">
+      <ProjectsGrid />
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
