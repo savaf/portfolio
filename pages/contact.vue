864c29fbd1ea449dec18df349b50ebeec33acd79
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 22:50:51 -0400

    Update contact page template

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,1 +1,9 @@
-<template>contacto</template>
+<script></script>
+
+<template>
+  <main class="isolate w-full bg-secondary-dark mt-16 sm:mt-32 pb-16 sm:pb-32">
+    <div class="container">contact</div>
+  </main>
+</template>
+
+<style scoped></style>
