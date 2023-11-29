1b9b39d20c76a8ff1f5cd77a9aa8c1fa3b33eab1
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 20:36:04 -0400

    Update font sizes in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -39,60 +39,60 @@ export default <Partial<Config>>{
         display: ["Montserrat", ...defaultTheme.fontFamily.sans],
         loading: ["Flow Circular", "cursive"],
       },
-      fontSize: {
-        base: [
-          "16px",
-          {
-            fontWeight: "400",
-            lineHeight: "24px",
-          },
-        ],
-        xs: "12px",
-        sm: "14px",
-        md: "16px",
-        lg: "18px",
-        xl: "20px",
-        "display-xs": [
-          "24px",
-          {
-            lineHeight: "32px",
-          },
-        ],
-        "display-sm": [
-          "30px",
-          {
-            lineHeight: "38px",
-          },
-        ],
-        "display-md": [
-          "36px",
-          {
-            lineHeight: "44px",
-            letterSpacing: "-0.72px",
-          },
-        ],
-        "display-lg": [
-          "48px",
-          {
-            lineHeight: "60px",
-            letterSpacing: "-0.96px",
-          },
-        ],
-        "display-xl": [
-          "60px",
-          {
-            lineHeight: "72px",
-            letterSpacing: "-1.2px",
-          },
-        ],
-        "display-2xl": [
-          "72px",
-          {
-            lineHeight: "90px",
-            letterSpacing: "-1.44px",
-          },
-        ],
-      },
+      // fontSize: {
+      //   base: [
+      //     "16px",
+      //     {
+      //       fontWeight: "400",
+      //       lineHeight: "24px",
+      //     },
+      //   ],
+      //   xs: "12px",
+      //   sm: "14px",
+      //   md: "16px",
+      //   lg: "18px",
+      //   xl: "20px",
+      //   "display-xs": [
+      //     "24px",
+      //     {
+      //       lineHeight: "32px",
+      //     },
+      //   ],
+      //   "display-sm": [
+      //     "30px",
+      //     {
+      //       lineHeight: "38px",
+      //     },
+      //   ],
+      //   "display-md": [
+      //     "36px",
+      //     {
+      //       lineHeight: "44px",
+      //       letterSpacing: "-0.72px",
+      //     },
+      //   ],
+      //   "display-lg": [
+      //     "48px",
+      //     {
+      //       lineHeight: "60px",
+      //       letterSpacing: "-0.96px",
+      //     },
+      //   ],
+      //   "display-xl": [
+      //     "60px",
+      //     {
+      //       lineHeight: "72px",
+      //       letterSpacing: "-1.2px",
+      //     },
+      //   ],
+      //   "display-2xl": [
+      //     "72px",
+      //     {
+      //       lineHeight: "90px",
+      //       letterSpacing: "-1.44px",
+      //     },
+      //   ],
+      // },
       borderRadius: {
         "4xl": "2rem",
         "5xl": "3rem",
