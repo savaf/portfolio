fa307d57edd7be7d8391ba308cfe18620da6a372
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 22:50:27 -0400

    Refactor project grid and related projects components

--- components/projects/Grid.vue
+++ components/projects/Grid.vue
@@ -36,41 +36,48 @@ function filterProjectsBySearch() {
   <div class="pt-10 sm:pt-20 md:pt-24">
     <!-- Projects grid header -->
     <div class="text-center">
-      <p class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light">
+      <p class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-light">
         {{ projectsHeading }}
       </p>
       <!-- Note: This description is commented out, but if you want to see it, just uncomment this -->
-      <!-- <p class="text-lg sm:text-xl text-gray-500 dark:text-ternary-light">
+      <p class="text-lg sm:text-xl text-gray-500 dark:text-ternary-light">
         {{ projectsDescription }}
-      </p> -->
+      </p>
     </div>
 
     <!-- Filter and search projects -->
-    <div class="mt-8 sm:mt-10">
-      <h3 class="font-general-regular text-center text-secondary-dark dark:text-ternary-light text-md sm:text-xl font-normal mb-4">Search projects by title or filter by category</h3>
-      <div class="flex justify-between border-b border-primary-light dark:border-secondary-dark pb-3 gap-2">
+    <form class="mt-8 sm:mt-10">
+      <h3 class="font-general-regular text-center text-ternary-light text-md sm:text-xl font-normal mb-4">Search projects by title or filter by category</h3>
+      <div class="flex justify-between border-b border-secondary-dark pb-3 gap-2">
         <div class="flex justify-between gap-2">
-          <span class="hidden sm:block bg-primary-light dark:bg-ternary-dark p-2.5 shadow-sm rounded-xl cursor-pointer">
-            <i data-feather="search" class="text-ternary-dark dark:text-ternary-light"></i>
-          </span>
-          <input v-model="searchProject" class="font-general-medium pl-3 pr-1 sm:px-4 py-2 border-1 border-gray-200 dark:border-secondary-dark rounded-lg text-sm sm:text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light" id="name" name="name" type="search" required="" placeholder="Search Projects" aria-label="Name" />
+          <button type="button" @click="filterProjectsBySearch" class="hidden sm:block bg-ternary-dark p-2.5 shadow-sm rounded-xl cursor-pointer">
+            <Icon name="i-heroicons-magnifying-glass" class="w-6 h-6 text-ternary-light" aria-hidden="true" />
+          </button>
+          <input v-model="searchProject" class="font-general-medium pl-3 pr-1 sm:px-4 py-2 border-1 border-secondary-dark rounded-lg text-sm sm:text-md bg-ternary-dark text-ternary-light" id="name" name="name" type="search" required placeholder="Search Projects" aria-label="Name" @keyup.enter="filterProjectsBySearch" />
         </div>
         <ProjectsFilter @change="selectedProject = $event" />
       </div>
-    </div>
+    </form>
 
     <!-- Projects grid -->
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
-      <div v-for="project in filteredProjects" :key="project.id" class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark" aria-label="Single Project">
+      <div v-for="project in filteredProjects" :key="project.id" class="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-ternary-dark" aria-label="Single Project">
         <NuxtLink :to="`/projects/${project.id}`">
           <div>
-            <img :src="project.img" :alt="project.title" class="rounded-t-xl border-none" />
+            <NuxtPicture
+              :src="project.img"
+              :alt="project.title"
+              format="webp,jpg"
+              :img-attrs="{
+                class: 'w-full h-48 sm:h-56 object-cover object-center rounded-t-xl',
+              }"
+            />
           </div>
           <div class="text-center px-4 py-6">
-            <p class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2">
+            <p class="font-general-semibold text-xl text-ternary-light font-semibold mb-2">
               {{ project.title }}
             </p>
-            <span class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light">{{ project.category }}</span>
+            <span class="font-general-medium text-lg text-ternary-light">{{ project.category }}</span>
           </div>
         </NuxtLink>
       </div>
--- components/projects/RelatedProjects.vue
+++ components/projects/RelatedProjects.vue
@@ -1,1 +1,47 @@
-<template>related projects</template>
+<script>
+export default {
+  data: () => {
+    return {
+      relatedProject: {
+        relatedProjectsHeading: "Related Projects",
+        relatedProjects: [
+          {
+            id: 1,
+            title: "Mobile UI",
+            img: "/images/mobile-project-1.jpg",
+          },
+          {
+            id: 2,
+            title: "Web Application",
+            img: "/images/web-project-1.jpg",
+          },
+          {
+            id: 3,
+            title: "UI Design",
+            img: "/images/ui-project-2.jpg",
+          },
+          {
+            id: 4,
+            title: "Kabul Mobile App UI",
+            img: "/images/mobile-project-2.jpg",
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="mt-10 pt-10 sm:pt-14 sm:mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
+    <p class="font-general-regular text-primary-dark dark:text-primary-light text-3xl font-bold mb-10 sm:mb-14 text-left">
+      {{ relatedProject.relatedProjectsHeading }}
+    </p>
+
+    <div class="grid grid-cols-1 sm:grid-cols-4 gap-10">
+      <div v-for="item in relatedProject.relatedProjects" :key="item.id">
+        <NuxtPicture :src="item.img" class="rounded-xl cursor-pointer" :alt="item.title" />
+      </div>
+    </div>
+  </div>
+</template>
