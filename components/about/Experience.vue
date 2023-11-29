229513faea594a8dbf5e0dac9f98917d9e700443
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-29 18:47:54 -0400

    Refactor jobExperiences data fetching

--- components/about/Experience.vue
+++ components/about/Experience.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import jobExperiences from "~/data/jobExperiences";
+const jobExperiences = await queryContent("experiences").find();
 </script>
 
 <template>
@@ -9,7 +9,7 @@ import jobExperiences from "~/data/jobExperiences";
     <ol class="relative border-s border-primary-light text-ternary-light">
       <li class="mb-10 ms-6" v-for="experience in jobExperiences">
         <span class="absolute flex items-center justify-center w-6 h-6 rounded-full -start-3 ring-8 ring-gray-900 bg-blue-900">
-          <Icon name="i-heroicons-check-circle-solid" class="w-6 h-6" aria-hidden="true" />
+          <Icon name="i-heroicons-briefcase-solid" class="w-6 h-6" aria-hidden="true" />
         </span>
         <h3 class="flex items-center mb-1 text-lg font-semibold text-white">
           {{ experience.position }} &nbsp; <span class="text-indigo-600">@{{ experience.company }}</span>
@@ -17,7 +17,9 @@ import jobExperiences from "~/data/jobExperiences";
         <span class="block mb-2 text-sm font-normal leading-none text-gray-500">
           <time>{{ experience.date }}</time> | {{ experience.location }}
         </span>
-        <p class="mb-4 text-base font-normal text-ternary-light">{{ experience.description }}</p>
+        <p v-for="p in experience.description" :key="p" class="mb-4 text-base font-normal text-ternary-light">
+          {{ p }}
+        </p>
       </li>
     </ol>
   </section>
