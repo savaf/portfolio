87bc8fdba3d8bc1daa5459f256e6f881e04d55a0
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 22:50:47 -0400

    Add project single page

---
+++ pages/projects/[id].vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <main class="isolate w-full bg-secondary-dark mt-16 sm:mt-32 pb-16 sm:pb-32">
+    <div class="container">project single</div>
+  </main>
+</template>
+
+<style scoped></style>
--- pages/projects/_id.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Project 1</h1>
-</template>
