7c43318e0aac9c3d579d1322974524dd7a54b9a4
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-29 04:42:02 -0400

    Update Bio and Hero components

--- components/about/Bio.vue
+++ components/about/Bio.vue
@@ -11,9 +11,9 @@ import socialLinks from "~/data/socialLinks";
           format="webp,jpg"
           width="316"
           height="316"
-          sizes="316px md:433px"
+          sizes="316px lg:433px"
           alt=""
-          class="w-full bg-zinc-800"
+          class="w-[316px] lg:w-[433px] bg-zinc-800"
           :img-attrs="{
             class: 'w-full aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover bg-zinc-800',
           }"
@@ -21,12 +21,11 @@ import socialLinks from "~/data/socialLinks";
       </div>
     </div>
     <div class="lg:order-first lg:row-span-2">
-      <h1 class="text-4xl font-bold tracking-tight text-ternary-light sm:text-5xl">I’m Spencer Sharp. I live in New York City, where I design the future.</h1>
+      <h1 class="text-4xl font-bold tracking-tight text-ternary-light sm:text-5xl">BIOGRAPHY</h1>
       <div class="mt-6 space-y-7 text-base text-ternary-light">
-        <p>I’ve loved making things for as long as I can remember, and wrote my first program when I was 6 years old, just two weeks after my mom brought home the brand new Macintosh LC 550 that I taught myself to type on.</p>
-        <p>The only thing I loved more than computers as a kid was space. When I was 8, I climbed the 40-foot oak tree at the back of our yard while wearing my older sister’s motorcycle helmet, counted down from three, and jumped — hoping the tree was tall enough that with just a bit of momentum I’d be able to get to orbit.</p>
-        <p>I spent the next few summers indoors working on a rocket design, while I recovered from the multiple surgeries it took to fix my badly broken legs. It took nine iterations, but when I was 15 I sent my dad’s Blackberry into orbit and was able to transmit a photo back down to our family computer from space.</p>
-        <p>Today, I’m the founder of Planetaria, where we’re working on civilian space suits and manned shuttle kits you can assemble at home so that the next generation of kids really can make it to orbit — from the comfort of their own backyards.</p>
+        <p>I’m Sinver Aguiló a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 7+ years of experience in the field. I am always looking for new and innovative ways to bring my clients' visions to life.</p>
+        <p>I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users.</p>
+        <p>Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
       </div>
     </div>
     <div class="lg:pl-20">
