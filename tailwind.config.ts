fa1b274b9f923fa0a69113f66f6710283e9dde30
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-29 06:09:02 -0400

    Add daisyui and flowbite dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ import defaultTheme from "tailwindcss/defaultTheme";
 import tailwindTypography from "@tailwindcss/typography";
 import tailwindForms from "@tailwindcss/forms";
 import tailwindAspectRatio from "@tailwindcss/aspect-ratio";
+// import daisyUI from "daisyui";
+// import daisyTheme from "daisyui/src/theming/themes";
+import flowbitePlugin from "flowbite/plugin";
 
 const srcDir = "./";
 export default <Partial<Config>>{
@@ -99,5 +102,27 @@ export default <Partial<Config>>{
       },
     },
   },
-  plugins: [tailwindForms, tailwindTypography, tailwindAspectRatio],
+  plugins: [tailwindForms, tailwindTypography, tailwindAspectRatio, flowbitePlugin],
+
+  // daisyUI config (optional - here are the default values)
+  daisyui: {
+    // themes: true, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    themes: ["aqua"],
+    // themes: [
+    //   {
+    //     light: {
+    //       ...daisyTheme["[data-theme=aqua]"],
+    //       "secondary-content": "#fff",
+    //       "accent-content": "#fff",
+    //     },
+    //   },
+    // ],
+    darkTheme: "aqua", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
 };
