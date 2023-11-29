8d49c8c76772b90cc2292504cda41052a323b969
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 20:35:46 -0400

    Add Filter, Grid, and RelatedProjects components

---
+++ components/projects/Filter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+  select: {
+    type: String,
+    default: "projects",
+  },
+  selectOptions: {
+    type: Array,
+    default: () => ["Web Application", "Mobile Application", "UI/UX Design", "Branding & Anim"],
+  },
+});
+
+const emit = defineEmits(["change"]);
+</script>
+
+<template>
+  <select @change="$emit('change', $event.target.value)" :name="select" :id="select" class="font-general-medium px-4 py-2 border-1 border-gray-200 dark:border-secondary-dark rounded-lg text-sm sm:text-md bg-secondary-light dark:bg-ternary-dark text-primary-dark dark:text-ternary-light">
+    <option value class="text-sm sm:text-md">All Projects</option>
+    <option v-for="option in selectOptions" :key="option" :value="option" class="sm:text-md">
+      {{ option }}
+    </option>
+  </select>
+</template>
---
+++ components/projects/RelatedProjects.vue
@@ -0,0 +1,1 @@
+<template>related projects</template>
