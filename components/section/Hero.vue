7867cb19257475017e535b8d302c91879477f5d1
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-28 22:19:11 -0400

    Update Hero section text color

--- components/section/Hero.vue
+++ components/section/Hero.vue
@@ -24,7 +24,7 @@
     <section class="container mx-auto flex flex-col sm:justify-between items-center sm:flex-row mt-12 sm:mt-10">
       <!-- Banner left contents -->
       <div class="w-full md:w-1/3 text-left">
-        <h1 class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-primary-light uppercase">Hi, I'm Sinver Aguilo</h1>
+        <h1 class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-light uppercase">Hi, I'm Sinver Aguilo</h1>
         <p class="font-general-medium mt-2 text-lg sm:text-xl xl:text-2xl text-center sm:text-left leading-none text-gray-400 drop-shadow-lg">Turning Your Vision Into Reality With Code And Design.</p>
         <div class="flex justify-center sm:block">
           <a href="/Sinver-Aguilo-CV.pdf" download="" class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500" aria-label="Download Resume">
