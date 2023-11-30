80a865faa1e740d60d453c4be0215b62d180a5e0
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-30 02:12:03 -0400

    Refactor social links in AppFooter.vue

--- components/shared/AppFooter.vue
+++ components/shared/AppFooter.vue
@@ -10,10 +10,12 @@ const { links: socialLinks } = await queryContent("social_links").findOne();
         <div class="flex flex-col justify-center items-center mb-12 sm:mb-20">
           <p class="font-general-semibold text-3xl sm:text-4xl text-primary-light mb-5">Follow me</p>
           <ul class="flex gap-4 sm:gap-8">
-            <NuxtLink v-for="social in socialLinks" :key="social.id" :href="social.url" target="__blank" class="text-gray-400 hover:text-indigo-400 cursor-pointer rounded-lg bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-500">
-              <span class="sr-only">{{ social.name }}</span>
-              <Icon :name="social.icon" class="w-6 sm:w-8 h-6 sm:h-8" aria-hidden="true" />
-            </NuxtLink>
+            <li v-for="social in socialLinks" :key="social.id">
+              <NuxtLink :href="social.url" target="__blank" class="text-gray-400 hover:text-indigo-400 cursor-pointer rounded-lg bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-500">
+                <span class="sr-only">{{ social.name }}</span>
+                <Icon :name="social.icon" class="w-6 sm:w-8 h-6 sm:h-8" aria-hidden="true" />
+              </NuxtLink>
+            </li>
           </ul>
         </div>
 
