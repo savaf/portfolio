4bfe91f7c2099f445ab59247a2e9a03b101f5d7a
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-30 00:04:55 -0400

    Delete unused project file and add new project JSON file

--- pages/projects/[id].vue
+++
@@ -1,9 +0,0 @@
-<script></script>
-
-<template>
-  <main class="isolate w-full bg-secondary-dark mt-16 sm:mt-32 pb-16 sm:pb-32">
-    <div class="container">project single</div>
-  </main>
-</template>
-
-<style scoped></style>
