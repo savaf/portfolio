02d1b89b606f3e86740004f5a1ccc2cbf1c237f2
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-30 09:26:34 -0400

    Fix image domain configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -88,7 +88,7 @@ export default defineNuxtConfig({
 
   image: {
     format: ["webp"],
-    domains: isDev ? ["localhost"] : ["sinveraguilo.com"],
+    // domains: isDev ? ["localhost"] : ["sinveraguilo.com"],
   },
 
   css: ["~/assets/styles/main.scss"],
