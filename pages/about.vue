f732f1b0c038918bf65e589520de78cd28a1ab85
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-29 23:22:58 -0400

    Refactor social links file structure

--- pages/about.vue
+++ pages/about.vue
@@ -1,18 +1,12 @@
-<script setup lang="ts">
-import socialLinks from "~/data/socialLinks";
-</script>
-
 <template>
-  <main class="isolate w-full pt-28 bg-secondary-dark mt-16 sm:mt-32 pb-16 sm:pb-32">
+  <main class="isolate w-full pt-28 bg-secondary-dark mt-16 sm:mt-32">
     <div class="container mx-auto">
       <AboutBio />
     </div>
     <AboutCounter />
-    <div class="container mx-auto mt-16 sm:mt-32">
-      <AboutExperience />
-    </div>
-    <div class="container mx-auto mt-16 sm:mt-32">
-      <AboutEducation />
+    <AboutExperience class="container mx-auto mt-16 sm:mt-32" />
+    <div class="bg-ternary-dark mt-16 sm:mt-32">
+      <AboutEducation class="container mx-auto py-16" />
     </div>
   </main>
 </template>
