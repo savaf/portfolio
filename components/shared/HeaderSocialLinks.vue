d26dc4bb68cba52cad37026d2b8ba5b15c681e6c
Sinver Aguilo <sinveraguilo@gmail.com>
2023-11-30 02:10:59 -0400

    Refactor social links in HeaderSocialLinks.vue

--- components/shared/HeaderSocialLinks.vue
+++ components/shared/HeaderSocialLinks.vue
@@ -4,9 +4,11 @@ const { links: socialLinks } = await queryContent("social_links").findOne();
 
 <template>
   <ul class="flex gap-2 sm:gap-4">
-    <NuxtLink v-for="social in socialLinks" :key="social.id" :href="social.url" target="__blank" class="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 px-2 py-2 cursor-pointer rounded-lg shadow-sm duration-500">
-      <span class="sr-only">{{ social.name }}</span>
-      <Icon :name="social.icon" class="w-6 h-6" aria-hidden="true" />
-    </NuxtLink>
+    <li v-for="social in socialLinks" :key="social.id">
+      <NuxtLink :href="social.url" target="__blank" class="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 px-2 py-2 cursor-pointer rounded-lg shadow-sm duration-500">
+        <span class="sr-only">{{ social.name }}</span>
+        <Icon :name="social.icon" class="w-6 h-6" aria-hidden="true" />
+      </NuxtLink>
+    </li>
   </ul>
 </template>
