4325c19fde18cc311d31c89c3f2abcfbb38d1aba
Sinver Aguilo <sinveraguilo@gmail.com>
2023-12-01 00:37:39 -0400

    Refactor code to use async/await syntax for querying content

--- pages/about.vue
+++ pages/about.vue
@@ -1,42 +1,28 @@
 <script setup lang="ts">
-// const { data } = await useAsyncData("aboutPageContent", async () => {
-//   const [
-//     // destructure the results
-//     bio,
-//     socialLinks,
-//     educations,
-//     jobExperiences,
-//   ] = await Promise.all([
-//     // perform all queries in parallel
-//     queryContent("/bio").findOne(),
-//     queryContent("social_links").findOne(),
-//     queryContent("education").find(),
-//     queryContent("experiences").find(),
-//   ]);
+const { data } = await useAsyncData("aboutPageContent", async () => {
+  const [
+    // destructure the results
+    bio,
+    socialLinks,
+    educations,
+    jobExperiences,
+  ] = await Promise.all([
+    // perform all queries in parallel
+    queryContent("/bio").findOne(),
+    queryContent("social_links").findOne(),
+    queryContent("education").find(),
+    queryContent("experiences").find(),
+  ]);
 
-//   return {
-//     bio: bio,
-//     socialLinks: socialLinks.links,
-//     educations: educations,
-//     jobExperiences: jobExperiences,
-//   };
-// });
+  return {
+    bio,
+    socialLinks: socialLinks.links,
+    educations,
+    jobExperiences,
+  };
+});
 
-const [
-  // destructure the results
-  bio,
-  { links: socialLinks },
-  educations,
-  jobExperiences,
-] = await Promise.all([
-  // perform all queries in parallel
-  queryContent("/bio").findOne(),
-  queryContent("social_links").findOne(),
-  queryContent("education").find(),
-  queryContent("experiences").find(),
-]);
-
-// const { bio, socialLinks, educations, jobExperiences } = data.value;
+const { bio, socialLinks, educations, jobExperiences } = data.value;
 </script>
 
 <template>
