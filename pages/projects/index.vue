83d744359c8560bc99fd83f64212960431374438
Sinver Aguilo <sinveraguilo@gmail.com>
2024-03-19 14:44:04 -0400

    Add tip calculator project and enable project section on homepage

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -3,7 +3,7 @@
 <template>
   <main class="isolate w-full bg-secondary-dark pb-16 md:pb-32">
     <div class="container mx-auto">
-      <!-- <ProjectsGrid /> -->
+      <ProjectsGrid />
     </div>
   </main>
 </template>
