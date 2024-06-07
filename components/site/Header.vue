60740426e085629a9ad3e38df071f1b5d5aa3968
Sinver Aguilo <sinveraguilo@gmail.com>
2024-06-07 19:55:34 -0400

    refactor: Improve background effects in default layout

--- components/site/Header.vue
+++ components/site/Header.vue
@@ -26,7 +26,7 @@ const navigation = computed(() => ([
 
 // Dock animation
 const { $anime } = useNuxtApp()
-const dockElement = ref<HtmlElement | null>(null)
+const dockElement = ref<HTMLElement | null>(null)
 
 function getDistance(v1, v2) {
   return Math.sqrt((v1.x - v2.x) ** 2 + (v1.y - v2.y) ** 2)
@@ -73,6 +73,7 @@ function initialDockState() {
   })
 }
 
+// Dark mode
 const colorMode = useColorMode()
 const isDark = computed({
   get() {
@@ -82,9 +83,13 @@ const isDark = computed({
     colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
   }
 })
+
+// mobilePopover
+const openMobilePopover = ref(false)
 </script>
 
 <template>
+  <!-- Mobile navigation -->
   <header class="fixed inset-x-0 bottom-3 z-50 hidden lg:block">
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div
@@ -117,6 +122,7 @@ const isDark = computed({
       </div>
     </nav>
   </header>
+  <!-- Mobile navigation -->
   <div class="fixed z-10 flex flex-col w-full bottom-8 lg:hidden">
     <div class="h-16">
       <div class="sm:px-8 w-full">
@@ -124,15 +130,49 @@ const isDark = computed({
           <div class="relative px-4 sm:px-8 lg:px-12">
             <div class="max-w-full mx-auto">
               <div class="relative flex gap-4">
-                <div class="flex justify-end flex-1 md:justify-center">
+                <div class="flex justify-end flex-1 md:justify-center" aria-label="Global Mobile">
                   <div class="pointer-events-auto">
-                    <SiteNavButton
-                      :item="{
-                        name: 'Menu',
-                        icon: 'i-line-md-menu',
+                    <UPopover
+                      v-model:open="openMobilePopover"
+                      :popper="{
+                        arrow: true,
+                        placement: 'top',
                       }"
-                      class="ease-in dock-item"
-                    />
+                      :ui="{
+                        background: 'bg-red-500 dark:bg-gray-900',
+                        ring: '',
+                        base: 'box-gen',
+                      }"
+                    >
+                      <SiteNavButton
+                        :item="{
+                          name: 'Menu',
+                          icon: 'i-line-md-menu',
+                        }"
+                        class="ease-in dock-item "
+                      />
+
+                      <template #panel>
+                        <div class="p-4">
+                          <nav aria-label="Global Mobile">
+                            <ul>
+                              <li
+                                v-for="item in navigation"
+                                :key="item.href"
+                              >
+                                <NuxtLink
+                                  :to="item.href"
+                                  class="block py-2 px-3 text-sm/6 font-semibold dark:text-white/90 dark:hover:text-white"
+                                  :class="item.class"
+                                >
+                                  {{ item.name }}
+                                </NuxtLink>
+                              </li>
+                            </ul>
+                          </nav>
+                        </div>
+                      </template>
+                    </UPopover>
                   </div>
                   <div class="ml-2">
                     <SiteNavButton
@@ -152,6 +192,50 @@ const isDark = computed({
       </div>
     </div>
   </div>
+
+  <!-- <HUIPopover v-slot="{ open }" class="relative">
+    <HUIPopoverButton
+      :class="open ? 'text-white' : 'text-white/90'"
+      class="text-sm/6 font-semibold text-white/50 focus:outline-none data-[active]:text-white data-[hover]:text-white data-[focus]:outline-1 data-[focus]:outline-white"
+    >
+      Solutions
+    </HUIPopoverButton>
+
+    <transition
+      enter-active-class="transition duration-200 ease-out"
+      enter-from-class="translate-y-1 opacity-0"
+      enter-to-class="translate-y-0 opacity-100"
+      leave-active-class="transition duration-150 ease-in"
+      leave-from-class="translate-y-0 opacity-100"
+      leave-to-class="translate-y-1 opacity-0"
+    >
+      <HUIPopoverPanel
+        class="divide-y divide-white/5 rounded-xl bg-white/5 text-sm/6 [--anchor-gap:var(--spacing-5)]"
+      >
+        <div class="p-3">
+          <a class="block rounded-lg py-2 px-3 transition hover:bg-white/5" href="#">
+            <p class="font-semibold text-white">Insights</p>
+            <p class="text-white/50">Measure actions your users take</p>
+          </a>
+          <a class="block rounded-lg py-2 px-3 transition hover:bg-white/5" href="#">
+            <p class="font-semibold text-white">Automations</p>
+            <p class="text-white/50">Create your own targeted content</p>
+          </a>
+          <a class="block rounded-lg py-2 px-3 transition hover:bg-white/5" href="#">
+            <p class="font-semibold text-white">Reports</p>
+            <p class="text-white/50">Keep track of your growth</p>
+          </a>
+        </div>
+        <div class="p-3">
+          <a class="block rounded-lg py-2 px-3 transition hover:bg-white/5" href="#">
+            <p class="font-semibold text-white">Documentation</p>
+            <p class="text-white/50">Start integrating products and tools</p>
+          </a>
+        </div>
+      </HUIPopoverPanel>
+    </transition>
+  </HUIPopover> -->
+  <!-- Blur effect -->
   <div class="fixed inset-x-0 bottom-0 isolate -z-10 h-[100px]">
     <div class="gradient-mask-t-0 absolute inset-0 backdrop-blur-[1px]" />
     <div class="gradient-mask-t-0 absolute inset-0 backdrop-blur-[2px]" />
@@ -159,14 +243,13 @@ const isDark = computed({
     <div class="gradient-mask-t-0 absolute inset-0 backdrop-blur-[6px]" />
     <div class="gradient-mask-t-0 absolute inset-0 backdrop-blur-[12px]" />
   </div>
-  <div class="" />
 </template>
 
 <style lang="css">
 .box-gen {
-  @apply border border-white bg-opacity-70 border-white
+  @apply border border-white bg-opacity-70
   bg-gray-50 outline outline-1 outline-gray-200 bg-blend-luminosity
-   backdrop-blur-[24px] dark:border-black/10 dark:outline-[#1a1a1a] dark:bg-zinc-900/20
+  backdrop-blur-[24px] dark:border-black/10 dark:outline-[#1a1a1a] dark:bg-zinc-900/20
   ;
   /* bg-blend-luminosity outline-solid outline border-gray-200
   backdrop-filter backdrop-blur-24 backdrop-brightness-100 backdrop-contrast-100
