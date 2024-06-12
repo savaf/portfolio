986fcb1d8b27ef7ddea1fb355c6f9367ca29f5d0
Sinver Aguilo <sinveraguilo@gmail.com>
2024-06-12 02:31:18 -0400

    feat: Add Bento component to Landing page

---
+++ components/Landing/Bento.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  icon: {
+    name: string
+    class: string
+  }
+}>()
+</script>
+
+<template>
+  <div class="box-gen hover:shadow-lg group relative flex flex-col items-start rounded-2xl p-4 shadow overflow-hidden sm:col-span-3 md:col-span-3 lg:col-span-2">
+    <div class="body-primary box-gen line-clamp-2 inline-flex h-8 items-center gap-1 rounded-2xl px-4 text-sm leading-5 z-10 mb-0">
+      <UIcon :name="icon.name" class="w-4 h-4 flex-none" :class="icon.class" />
+      <span>{{ title }}</span>
+    </div>
+    <div class="mt-4">
+      <slot />
+    </div>
+  </div>
+</template>
