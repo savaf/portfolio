fde271b52ca192e0c85b209985c6ee840ce1924d
Sinver Aguilo <sinveraguilo@gmail.com>
2024-06-13 05:47:10 -0400

    refactor: Improve background effects in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,9 +5,9 @@
 <template>
   <!-- Background Effects -->
   <div
-    class="fixed inset-0 -z-10 h-full w-full "
+    class="fixed inset-0 -z-10 h-full w-full blur-sm"
   >
-    <!-- <NuxtImg
+    <NuxtImg
       class="absolute inset-0 -z-10 h-full w-full object-cover hidden dark:block"
       src="/images/background_dark.jpg"
       alt=""
@@ -16,7 +16,7 @@
       class="absolute inset-0 -z-10 h-full w-full object-cover block dark:hidden"
       src="/images/background_light.jpg"
       alt=""
-    /> -->
+    />
     <div class="absolute inset-x-0 top-10 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl" aria-hidden="true">
       <div class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" />
     </div>
@@ -25,7 +25,7 @@
     <!-- Header -->
     <SiteHeader />
 
-    <div>
+    <div class="pt-16 sm:pt-32">
       <slot />
     </div>
 
