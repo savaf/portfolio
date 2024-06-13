3aa1449d3479033ea65158817190409c8288d4b1
Sinver Aguilo <sinveraguilo@gmail.com>
2024-06-13 05:47:55 -0400

    refactor: Rename Bento.vue to Box.vue and update component props and template

--- components/Landing/Box.vue
+++ components/Landing/Box.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 defineProps<{
-  title: string
-  icon: {
+  title?: string
+  icon?: {
     name: string
     class: string
   }
@@ -10,11 +10,11 @@ defineProps<{
 
 <template>
   <div class="box-gen hover:shadow-lg group relative flex flex-col items-start rounded-2xl p-4 shadow overflow-hidden sm:col-span-3 md:col-span-3 lg:col-span-2">
-    <div class="body-primary box-gen line-clamp-2 inline-flex h-8 items-center gap-1 rounded-2xl px-4 text-sm leading-5 z-10 mb-0">
-      <UIcon :name="icon.name" class="w-4 h-4 flex-none" :class="icon.class" />
+    <div v-if="title" class="body-primary box-gen line-clamp-2 inline-flex h-8 items-center gap-1 rounded-2xl px-4 text-sm leading-5 z-10 mb-0">
+      <UIcon v-if="icon" :name="icon.name" class="w-4 h-4 flex-none" :class="icon.class" />
       <span>{{ title }}</span>
     </div>
-    <div class="mt-4">
+    <div class="mx-auto" :class="[title ? 'mt-4' : '']">
       <slot />
     </div>
   </div>
